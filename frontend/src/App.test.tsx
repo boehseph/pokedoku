bfd1446010f6a0008df33efebbbd35688b766d81
Boeh Remtulla <boeh.remtulla@gmail.com>
2026-04-04 23:51:21 -0400

    Got rid of unnecessary elements

--- frontend/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders PokéDoku heading', () => {
-  render(<App />);
-  expect(screen.getByText(/PokéDoku/i)).toBeInTheDocument();
-});
